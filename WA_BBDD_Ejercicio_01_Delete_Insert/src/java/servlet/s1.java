0b86e8ec128ae471bdaa4acef3807456872eef96
FMHJ97 <francisco-h-j@hotmail.com>
2024-11-18 14:55:04 +0100

    feat(HLC): WA_BBDD_Ejercicio_01_Delete_Insert

---
+++ WA_BBDD_Ejercicio_01_Delete_Insert/src/java/servlet/s1.java
@@ -0,0 +1,131 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package servlet;
+
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.ConnMysql;
+
+/**
+ *
+ * @author DIURNO
+ */
+@WebServlet(name = "s1", urlPatterns = {"/s1"})
+public class s1 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>BD uno - Ejercicio 01</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<form action='#' method='POST'>");
+            
+            try {
+                // Creamos el objeto conexion
+                Connection conn = new ConnMysql().getConnection();
+                // Creamos un objeto Statement
+                Statement instruccion = conn.createStatement(
+                        ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+                // Creamos el query
+                String sql = "select * from t1";
+                ResultSet rs = instruccion.executeQuery(sql);
+                // Creamos una fila por cada registro.
+                while (rs.next()) {
+                    out.println("<p>");
+                    out.println(rs.getString(2) + " ");
+                    out.println(rs.getString(3));
+                    out.println("<button name='button' value='" + rs.getInt(1) + "'>Borrar</button>");
+                    out.println("</p>");
+                }
+                
+                // Cerrar cada uno de los objetos utilizados
+                rs.close();
+                instruccion.close();
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            
+            out.println("</form>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(s1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(s1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
